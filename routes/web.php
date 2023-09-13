05abe5732d3927d37a20fc8f445707c311e6f818
xcyltra <luludea95@gmail.com>
2023-09-13 13:35:16 +0800

    Membuat Halaman Web ketika Pertama Kali di Buka adalah Login

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use App\Http\Controllers\PenilaianKerjaController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Route::get('/dashboard', function () {
